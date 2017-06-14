80060fdd3da8c368c07edae8ef5f4f706bc3c4c1
Daniel Noonan <dannoonan99@gmail.com>
2017-06-14 12:10:11 +0100

    Flex-nav: mouse enter event, styles

--- app/assets/script/script.js
+++ app/assets/script/script.js
@@ -9,13 +9,13 @@ let levelOneArr = Array.from(document.querySelectorAll('.has-sub-menu')),
     //create array of level 2 sub-menus
     levelTwoArr = Array.from(document.querySelectorAll('.ul-level-2')),
     //create array of all top tier menu items
-    topTier = Array.from(document.querySelectorAll('.top-tier'));
+    topTier = Array.from(document.querySelectorAll('.js-top-tier'));
 
 ////////////////
 //iterate of the array of all top tier menu items
 topTier.forEach(item => {
     //add event listeners to each of them
-    item.addEventListener('click', () => {
+    item.addEventListener('mouseenter', () => {
         //if one of the menu items that does NOT have a sub-menu is clicked then close/hide any open sub menues
         if (item.classList.contains('has-sub-menu') === false) {
             levelTwoArr.forEach(levelTwo => {
@@ -29,7 +29,7 @@ topTier.forEach(item => {
 //iterate over the array of level 1 items that have sub-menus
 levelOneArr.forEach(item => {
     //add event listeners to each of them
-    item.addEventListener('click', () => {
+    item.addEventListener('mouseenter', () => {
         //store the array index of the level 1 item clicked
         let index = levelOneArr.indexOf(item);
         //store the level 2 sub-menu that corresponds to the same array index value and the index value of the level 1 array that was clicked
@@ -44,37 +44,3 @@ levelOneArr.forEach(item => {
         subMenu.classList.toggle('hide-sub-menu');
     })
 })
-
-
-
-
-
-////un-commented version
-//let levelOneArr = Array.from(document.querySelectorAll('.has-sub-menu'));
-//let levelTwoArr = Array.from(document.querySelectorAll('.ul-level-2'));
-//
-//
-//let topTier = Array.from(document.querySelectorAll('.top-tier'));
-//topTier.forEach(item => {
-//    item.addEventListener('click', () => {
-//        if (item.classList.contains('has-sub-menu') === false) {
-//            levelTwoArr.forEach(levelTwo => {
-//                levelTwo.classList.add('hide-sub-menu');
-//            });
-//        }
-//    });
-//});
-//
-//
-//levelOneArr.forEach(item => {
-//    item.addEventListener('click', () => {
-//        let index = levelOneArr.indexOf(item);
-//        let subMenu = levelTwoArr[index];
-//        levelTwoArr.forEach(item => {
-//            if (item.classList.contains('hide-sub-menu') === false && levelTwoArr.indexOf(item) != index) {
-//                item.classList.add('hide-sub-menu');
-//            }
-//        });
-//        subMenu.classList.toggle('hide-sub-menu');
-//    });
-//});
