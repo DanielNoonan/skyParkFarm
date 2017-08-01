8edf15f119b0f7626b4d9cc8d7605aab3f5a91d0
Daniel Noonan <dannoonan99@gmail.com>
2017-08-01 16:34:47 +0100

    Completely removed flexnav and replaced html/css/javaScript with all new code because previous version had issues on Mac iOS

--- app/assets/script/script.js
+++ app/assets/script/script.js
@@ -1,66 +1,67 @@
 'use strict'
-//////////////////////////////
-//Toggle level two sub-menus//
-//////////////////////////////
 
+//MOBILE TOGGLER
+//Toggles the mobile dropdown menu open and closed
 
-//Variables:
-//create array of level 1 menu items that have sub-menus
-let levelOneArr = Array.from(document.querySelectorAll('.flex-nav__has-sub-menu')),
-    //create array of level 2 sub-menus
-    levelTwoArr = Array.from(document.querySelectorAll('.flex-nav__ul-level-2')),
-    //create array of all top tier menu items
-    topTier = Array.from(document.querySelectorAll('.js-top-tier'));
-
-////////////////
-//iterate over the array of all top tier menu items
-topTier.forEach(item => {
-    //add event listeners to each of them
-    item.addEventListener('click', () => {
-        //if one of the menu items that does NOT have a sub-menu is clicked then close/hide any open sub menues
-        if (item.classList.contains('flex-nav__has-sub-menu') === false) {
-            closeSubMenus();
-        }
-    });
+document.querySelector('.flexnav__toggler').addEventListener('click', () => {
+    document.querySelector('.flexnav__toptab-ul').classList.toggle('mobile-nav-revealer');
+    toggleButton();
 });
 
-const closeSubMenus = () => {
-    levelTwoArr.forEach(levelTwo => {
-        levelTwo.classList.add('js-hide-sub-menu');
-    });
+
+//Changes the toggle button icon between a '+' and an 'x'
+
+const toggleButton = () => {
+    let toggle = document.querySelector('.flexnav__toggler').innerHTML
+    if (toggle === '+') {
+        document.querySelector('.flexnav__toggler').innerHTML = '&times;';
+    } else {
+        document.querySelector('.flexnav__toggler').innerHTML = '&#43;';
+    }
 }
 
-////////////////
-//iterate over the array of level 1 items that have sub-menus
-levelOneArr.forEach(item => {
-    //add event listeners to each of them
-    item.addEventListener('click', () => {
-        //store the array index of the level 1 item clicked
-        let index = levelOneArr.indexOf(item);
-        //store the level 2 sub-menu that corresponds to the same array index value and the index value of the level 1 array that was clicked
-        let subMenu = levelTwoArr[index];
-        //iterate over the level 2 sub-menu array and close any that are open - before the next step of openning the one clicked
-        levelTwoArr.forEach(item => {
-            if (item.classList.contains('js-hide-sub-menu') === false && levelTwoArr.indexOf(item) != index) {
-                item.classList.add('js-hide-sub-menu');
-            }
-        });
-        //open/reveal the level 2 sub-menu
-        subMenu.classList.toggle('js-hide-sub-menu');
-    })
-})
 
 
-////////////////
-//Closes any open sub-menus if anywhere outside of the nav menu list is clicked
-let flexNavUnorderedList = document.querySelector('.js__click-outside-to-close');
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//Submenu Reset: on click outside navbar.
+//
+//Closes Mobile view dropdown menu if anywhere outside of the navbar (.flexnav) is clicked.
+
+let flexNavUnorderedList = document.querySelector('.flexnav');
 
 document.addEventListener('click', function (event) {
     let isClickInside = flexNavUnorderedList.contains(event.target);
     if (!isClickInside) {
-        closeSubMenus();
+        document.querySelector('.flexnav__toggler').innerHTML = '&#43;';
+        document.querySelector('.flexnav__toptab-ul').classList.remove('mobile-nav-revealer');
     }
 });
+
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+//Submenu Reset: on changing browser/screen size.
+//
+//javaScript Media Query to reset toggle button to a + when screen size enlarges.
+//Also it removes the '.mobile-nav-revealer' class from the '.flexnav__toptab-ul'.
+//By doing this the state of the submenu and toggle button are 'reset' so it's not left open if someone is e.g. dragging the side of the browser window between small and large width repeatedly while the mobile submenu is open (now it will close automatically on screen resize).
+
+let mq = window.matchMedia('(min-width: 1024px)');
+
+mq.addListener((changed) => {
+    if (changed.matches) {
+        document.querySelector('.flexnav__toggler').innerHTML = '&#43;';
+        document.querySelector('.flexnav__toptab-ul').classList.remove('mobile-nav-revealer');
+    }
+});
+
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
 
 
 //MODALS//
@@ -86,7 +87,7 @@ modalButtonArray.forEach(item => {
 
 
 modalButtonClose.forEach(item => {
-//    let modalsArray = Array.from(document.querySelectorAll('.modal'));
+    //    let modalsArray = Array.from(document.querySelectorAll('.modal'));
     item.addEventListener('click', () => {
         modalsArray[modalButtonClicked].classList.add('modal--hidden');
     });
@@ -96,7 +97,7 @@ modalButtonClose.forEach(item => {
 //PRINTING MODALS
 
 modalPrintButton.forEach(item => {
-//    let modalsArray = Array.from(document.querySelectorAll('.modal'));
+    //    let modalsArray = Array.from(document.querySelectorAll('.modal'));
     item.addEventListener('click', () => {
         modalsArray[modalButtonClicked].classList.add('printThis');
         printContent();
